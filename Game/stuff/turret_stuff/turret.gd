1fdead32908107f381ec4f774041778f5ddcc5aa
Ktosox <ktoso3@gmail.com>
2024-04-18 19:23:21 +0200

    some prgroess with turret

--- Game/stuff/turret_stuff/turret.gd
+++ Game/stuff/turret_stuff/turret.gd
@@ -27,6 +27,40 @@ var bonus_range
 
 var tooltip_scene = preload("res://ui/turret_tooltip.tscn")
 
+func _process(delta):
+	battery_current += delta * charge_rate
+	battery_current = min(battery_current, battery_max)
+
+
+func assemble_turret():
+	# place base
+	# place barrel
+	# connect signal from barrel to "shots_fired"
+	# get Funcref from barrel for turning it on/off
+	pass
+
+func shots_fired():
+	battery_current -= cost
+	if battery_current < cost:
+		$EnergyChecker.start()
+		# call Funcref from barrel to turn it off
+	pass
 
 
 
+
+func _on_EnergyChecker_timeout():
+	if battery_current > cost:
+		$EnergyChecker.stop()
+		# call Funcref from barrel to turn it on
+	pass # Replace with function body.
+
+
+func _on_HoverBox_mouse_entered():
+	$TurretTooltip.visible = true
+	pass # Replace with function body.
+
+
+func _on_HoverBox_mouse_exited():
+	$TurretTooltip.visible = false
+	pass # Replace with function body.
