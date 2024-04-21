5c0f24d3e29c6a14da3d2bb414291be7d9cca687
Ktosox <ktoso3@gmail.com>
2024-04-21 10:07:06 +0200

    improved shotgun feel

--- Game/stuff/turret_stuff/ammo/shotgun.gd
+++ Game/stuff/turret_stuff/ammo/shotgun.gd
@@ -8,9 +8,9 @@ var shell_scene = preload("res://stuff/turret_stuff/ammo/shell.tscn")
 func shoot():
 	for i in range(5):
 		var new_shell = shell_scene.instance()
-		new_shell.global_position = global_position
-		var speed = 560
-		var offset = rand_range(-1,1) * spread
-		new_shell.apply_central_impulse(Vector2.RIGHT.rotated(global_rotation + offset ) * speed)
+		new_shell.global_position = global_position + Vector2(rand_range(-4,4),rand_range(-4,4)) * spread
+		var speed = 690 + rand_range(-50,100)
+		var offset = rand_range(-0.2,0.2) * spread
+		new_shell.apply_central_impulse(Vector2.RIGHT.rotated(global_rotation + offset ) * speed )
 		get_tree().current_scene.add_child(new_shell)
 	pass
